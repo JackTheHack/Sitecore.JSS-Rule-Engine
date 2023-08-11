2a1d7f465fa81a7fd3e74753e3f10280fbb91f69
Jack Spektor <jack.spektor@akqa.com>
2023-08-11 14:49:34 +1200

    Added AVA unit test setup

--- test.js
+++ test.js
@@ -1,17 +1,25 @@
+
+var test = require('ava')
+
 var ruleEngine = require('./ruleEngine')
-var xmlStub = require('./ruleXmlStub')
+var xmlStub = require('./ruleXmlStub')    
+var ItemProvider = require("./itemProvider").ItemProvider;
 
-const ItemProvider = require("./itemProvider").ItemProvider;
+test('Parse and run rule', t => {
 
-console.log('Starting test.');
+    console.log('Starting test.');
+    
+    var itemProviderOptions = {
+    
+    };
+    var itemProvider = new ItemProvider(itemProviderOptions);
+    
+    var contextItem = {};
+    
+    var ruleResult = ruleEngine.runRule(contextItem, xmlStub, itemProvider);
+    
+    console.log('Rule result - ', ruleResult);
 
-var itemProviderOptions = {
+    t.pass();
+});
 
-};
-var itemProvider = new ItemProvider(itemProviderOptions);
-
-var contextItem = {};
-
-var ruleResult = ruleEngine.runRule(contextItem, xmlStub, itemProvider);
-
-console.log('Rule result - ', ruleResult);
